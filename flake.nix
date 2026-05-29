11bd3985cd5cd760f7f1b4d0eb4ce493fa3a73ca
Wesley Van Neck <wesleyvanneck550@gmail.com>
2026-05-29 12:23:56 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Minimal NixOS installation media";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-stable";
     # hyprland.url = "github:hyprwm/Hyprland";
 
     # nixos-muvm-fex = {
